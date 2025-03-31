de0b2558910eed84731d925ef7ca2882f5022a72
Hein Htet <hh1716101@gmail.com>
2025-04-01 00:48:03 +0700

    pointers

--- main.go
+++ main.go
@@ -2,28 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+func updateVal(x *string) {
+	*x = "Aung"
+}
 
+func main() {
 	name := "Hein Htet"
 
-	var address = &name
-	var memoName = *address
+	memo_address := &name
 
-	name = "Hein Htet Aung"
+	fmt.Println("Current :", name)
 
-	fmt.Println("Memo Address", address)
-	fmt.Println(memoName)
-	fmt.Println(name)
+	updateVal(memo_address)
 
-	x := []int{1, 2, 3, 4, 5}
-
-	fmt.Println(x[:3])
-	// menu := map[string][string]{
-	// 	"1": "One",
-	// }
-
-	fmt.Println(x)
-
-	// getting.helloworld("HEIN") // [1 2 3 4 5]
-	// helloworld("HEIN") // [1 2 3 4 5]
+	fmt.Println("Updated :", name)
 }
